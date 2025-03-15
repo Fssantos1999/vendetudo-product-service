5a7ac1b948356895673a5ee46b3ea626cfff0ad0
Felipe <felipecunhassantos@gmail.com>
2025-03-15 15:15:32 -0300

    refactor: ajusta UserMapper para compatibilidade com UpdateUserDto

--- marketplace/src/main/java/br/com/vendetudo/marketplace/modules/user/Mapper/UserMapper.java
+++ marketplace/src/main/java/br/com/vendetudo/marketplace/modules/user/Mapper/UserMapper.java
@@ -1,31 +1,36 @@
 package br.com.vendetudo.marketplace.modules.user.Mapper;
+import br.com.vendetudo.marketplace.modules.user.DTO.UpdateUserDto;
 import br.com.vendetudo.marketplace.modules.user.DTO.UserDTO;
 import br.com.vendetudo.marketplace.modules.user.Entity.UserEntity;
+import org.hibernate.sql.Update;
 import org.mapstruct.*;
 import java.util.List;
 
-@Mapper(componentModel = "spring")
 
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     @Mapping(target = "viaCepResponse", source = "viaCepResponse")
-    UserDTO userToUserDto(UserEntity user); // Converte Entidade → DTO
+    UserDTO userToUserDto(UserEntity user);
 
     @InheritInverseConfiguration
-    UserEntity userDtoToUser(UserDTO userDto); // Converte DTO → Entidade
+    UserEntity userDtoToUser(UserDTO userDto);
 
     @IterableMapping(elementTargetType = UserDTO.class)
     List<UserDTO> userToUserDto(List<UserEntity> user);
 
     @IterableMapping(elementTargetType = UserEntity.class)
-    List <UserEntity> userDtoToUserEntity(List<UserDTO> userDTOS);
+    List<UserEntity> userDtoToUserEntity(List<UserDTO> userDTOS);
 
-    //* atualização parcial do usuario
-   @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void dtoParaUsuarioAtualizado(UserDTO userDTO, @MappingTarget UserEntity userEntity); //! atualizar usuario
-//    @Mapping(source = "name", target = "name")
-//    UserDTO userEntityToUserDto(UserEntity userEntity);
+    //* Atualização parcial do usuário
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void dtoParaUsuarioAtualizado(UpdateUserDto updateUserDto, @MappingTarget UserEntity userEntity);
 
+    @Mapping(target = "viaCepResponse", ignore = true)
+    @Mapping(target = "gender", ignore = true)
+    @Mapping(target = "cep", ignore = true)
+    UserEntity toEntity(UpdateUserDto updateUserDto);
 
-
+    UpdateUserDto toDto(UserEntity userEntity);
 }
+
